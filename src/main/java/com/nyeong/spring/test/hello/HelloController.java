8fa1580e73fa5870c240f6e76646911fdf27574b
DESKTOP-8OLL720\82105 <rkdtnsudksn@naver.com>
2024-08-11 17:23:45 +0900

    [녕] - Spring test 최초 프로젝트 생성

---
+++ src/main/java/com/nyeong/spring/test/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.nyeong.spring.test.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/helloTest")
+	@ResponseBody
+	public String hello() {
+		return "Hello test";
+	}
+}
